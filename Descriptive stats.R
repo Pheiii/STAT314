19fec5b6ac21e8bf327353bf2c7b972682ddddb8
Pheiii <pburnside@claflin.edu>
2025-09-09 21:32:57 -0400

    Add files via upload

---
+++ Descriptive stats.R
@@ -0,0 +1,14 @@
+#Author: Pawar: Date: AUg 28 2025: Purpose: Measures of descriptive stats
+
+#Calculate mean/ average for data
+#Generate dummy/ sample dataset
+data<-c(1:50)
+
+#Calculate mean/average for data
+mean (data)
+
+#Calculate mean/average for data
+median (data)
+
+#Calculate standard deviation for data
+sd(data)
